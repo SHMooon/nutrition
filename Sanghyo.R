c1deb1b10b8e47893ad89bfd9ee601cfb5a911c1
SHMooon <SHMooon@users.noreply.github.com>
2023-05-19 13:04:20 +0200

    add

---
+++ Sanghyo.R
@@ -0,0 +1,12 @@
+#Plot the impact pathway
+library(decisionSupport)
+library(DiagrammeR)
+#1
+
+mermaid("graph LR
+        G(Gorvernment)-->T(Tax); linkStyle 0 stroke:green, stroke-width:1.5px
+        T--> F(Final result: Sugar reduction); linkStyle 1 stroke: green, stroke-width:1.5px
+        S(Supermarket)-->A(Arrange of Products); linkStyle 2 stroke: green, stroke-width:1.5px
+        A -->F; linkStyle 3 stroke: green, stroke-width:1.5px
+        I(Individuals)-->E(Education); linkStyle 4 stroke: green, stroke-width:1.5px
+        E-->F; linkStyle 5 stroke: green, stroke-width:1.5px")
