af971a101870e49fe9452e974a32be2b09ec6a04
Patrick Frey <paddyfrey@googlemail.com>
2023-05-19 13:20:22 +0200

    r into rmd

--- model_patrick.R
+++ model_patrick.R
@@ -6,7 +6,7 @@ library(decisionSupport)
 # install.packages("DiagrammeR")
 library(DiagrammeR)
 
-mermaid("graph LR
+model_patrick <- mermaid("graph LR
         G(Goverment)-->SR(Sugar Reduction); linkStyle 0 stroke: green, stroke-width:1.5px
         S(Supermarket)-->SR; linkStyle 1 stroke: red, stroke-width:1.5px
         I(Individual)-->SR; linkStyle 2 stroke: red, stroke-width:1.5px")
