ad84fba6610d221ddcf3aef08039fb7d442bfa27
SHMooon <SHMooon@users.noreply.github.com>
2023-05-19 13:17:24 +0200

    add

--- Sanghyo.R
+++ Sanghyo.R
@@ -3,7 +3,7 @@ library(decisionSupport)
 library(DiagrammeR)
 #1
 
-mermaid("graph LR
+Sanghyo <- mermaid("graph LR
         G(Gorvernment)-->T(Tax); linkStyle 0 stroke:green, stroke-width:1.5px
         T--> F(Final result: Sugar reduction); linkStyle 1 stroke: green, stroke-width:1.5px
         S(Supermarket)-->A(Arrange of Products); linkStyle 2 stroke: green, stroke-width:1.5px
