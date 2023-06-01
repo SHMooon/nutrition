dafa78915b45a0cde2e3cf825b4391ea733a2404
Patrick Frey <paddyfrey@googlemail.com>
2023-06-02 01:52:29 +0200

    subtable created try: fill ranges with distributions

--- collect_data.R
+++ collect_data.R
@@ -5,10 +5,20 @@ head(collected)
 library(tidyverse)
 
 # data wrangling
+# add a row 
+# new_row <- data.frame('004', 'gov', 'Another question', 14, 42, NA, "research")
+# write.table(new_row, file = "collect_data.csv", sep = ",", append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
+
+# create table for ploting
 collected_for_plot <- collected %>%
   select(question, lower.bound, upper.bound)
 
-
+# TODO answer ranges filled with distributions
+dis_answer001 <- rnorm(100); dis_answer001 <- dis_answer001[x > collected$lower.bound & x < collected$upper.bound]
+collected[1,] %>% 
+  rnorm(1)
+  
 # TODO plot 
 collected_for_plot %>% 
   ggplot() +
