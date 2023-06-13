338b804e798c59bc3f3f03d225d947c06bb3a2e3
Patrick Frey <paddyfrey@googlemail.com>
2023-06-13 17:17:33 +0200

    model created with R

--- model_patrick.R
+++ model_patrick.R
@@ -6,7 +6,63 @@ library(decisionSupport)
 # install.packages("DiagrammeR")
 library(DiagrammeR)
 
-model_patrick <- mermaid("graph LR
-        G(Goverment)-->SR(Sugar Reduction); linkStyle 0 stroke: green, stroke-width:1.5px
-        S(Supermarket)-->SR; linkStyle 1 stroke: red, stroke-width:1.5px
-        I(Individual)-->SR; linkStyle 2 stroke: red, stroke-width:1.5px")
+mermaid("graph LR
+        rc(Results of the Countries) --> sup(Support)
+        
+        gov(Goverment) --> sh(Stakeholders)
+        ind(Individuals) --> sh
+        sm(Supermarket) --> sh
+        man(Manufactories) --> sh
+        law(Curent state of law) --> gov
+        rn(Recommended nutrition by WHO) --> ind
+        int(Interests) --> sm
+        int --> man
+        sh --> sup
+        
+        in(Input) --> law
+        in(Input) --> rn
+        in(Input) --> int
+        
+        sup --> D(Decision)
+        D --> O(Outcome)
+        O --> G(Good)
+        O --> B(Bad)
+        
+        G --> in
+        B --> in
+        ")
+        
+?mermaid
+
+model_patrick
+
+grViz("digraph {
+  
+graph[layout = dot, rankdir = LR]
+
+O [label = 'Outcome']
+G [label = 'Good']
+B [label = 'Bad']   
+
+D [label = 'Decision']
+sup [label = 'Support']
+
+rc [label = 'Results of Countries with Taxes']
+gov [label = 'Goverment']
+ind [label = 'Individuals']
+sm [label = 'Supermarkets']
+man [label = 'Manufactories']
+sh [label = 'Stakeholder']
+
+gov, ind, sm, man -> sh -> sup
+rc -> sup
+sup -> D
+D -> O
+O -> G, B
+G, B -> sh
+
+}")
+
+
+
+?DiagrammeR
