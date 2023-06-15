42182d5a767ac64c097dd0273112d91e62ae38ed
Patrick Frey <paddyfrey@googlemail.com>
2023-06-15 16:51:41 +0200

    model with grViz

--- model_patrick.R
+++ model_patrick.R
@@ -6,65 +6,72 @@ library(decisionSupport)
 # install.packages("DiagrammeR")
 library(DiagrammeR)
 
-model_patrick <- mermaid("graph LR
-        rc(Results of the Countries) --> sup(Support)
-        
-        gov(Goverment) --> sh(Stakeholders)
-        ind(Individuals) --> sh
-        sm(Supermarket) --> sh
-        man(Manufactories) --> sh
-        law(Curent state of law) --> gov
-        rn(Recommended nutrition by WHO) --> ind
-        int(Interests) --> sm
-        int --> man
-        sh --> sup
-        
-        in(Input) --> law
-        in(Input) --> rn
-        in(Input) --> int
-        
-        sup --> D(Decision)
-        D --> O(Outcome)
-        O --> G(Good)
-        O --> B(Bad)
-        
-        G --> in
-        B --> in
-        ")
-        
-?mermaid
 
-model_patrick
-
-
-# old model ####
+# ipnut model with grViz ####
 grViz("digraph {
   
 graph[layout = dot, rankdir = LR]
+node[style = filled, margin = 0.1, fillcolor = 'azure']
 
-O [label = 'Outcome']
-G [label = 'Good']
-B [label = 'Bad']   
 
-D [label = 'Decision']
-sup [label = 'Support']
+I   [label = 'Input for Decision']
+pos [label = 'Positiv Support', fillcolor = 'green']
+neg [label = 'Negativ Support', fillcolor = 'red']
 
-rc [label = 'Results of Countries with Taxes']
-gov [label = 'Goverment']
-ind [label = 'Individuals']
-sm [label = 'Supermarkets']
-man [label = 'Manufactories']
-sh [label = 'Stakeholder']
+sh  [label = 'Stakeholder', shape = rectangle]
+res [label = 'Results of Countries', shape = rectangle]
 
-gov, ind, sm, man -> sh -> sup
-rc -> sup
-sup -> D
-D -> O
-O -> G, B
-G, B -> sh
+gov [label = 'Goverment', shape = rectangle]
+ind [label = 'Individuals', shape = rectangle]
+sm [label = 'Supermarkets', shape = rectangle]
+man [label = 'Manufactories', shape = rectangle]
+
+rs  [label = 'Research', shape = rectangle]
+
+gov, ind, sm, man -> sh -> pos, neg
+rs -> res
+res -> pos, neg 
+pos, neg -> I
 
 }")
 
 
 
-?DiagrammeR
+
+# 
+# model_patrick <- mermaid("graph LR
+#         rc(Results of the Countries) --> sup(Support)
+#         
+#         gov(Goverment) --> sh(Stakeholders)
+#         ind(Individuals) --> sh
+#         sm(Supermarket) --> sh
+#         man(Manufactories) --> sh
+#         law(Curent state of law) --> gov
+#         rn(Recommended nutrition by WHO) --> ind
+#         int(Interests) --> sm
+#         int --> man
+#         sh --> sup
+#         
+#         in(Input) --> law
+#         in(Input) --> rn
+#         in(Input) --> int
+#         
+#         sup --> D(Decision)
+#         D --> O(Outcome)
+#         O --> G(Good)
+#         O --> B(Bad)
+#         
+#         G --> in
+#         B --> in
+#         
+#         X(X) --> Y
+#         ")
+#         
+# ?mermaid
+# 
+# model_patrick
+# 
+
+
+
+# ?DiagrammeR
