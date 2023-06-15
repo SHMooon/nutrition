6449f81be53a5adfd9ac303384ca5d64afb9f3fe
Santosh Bhandari <san.bhandari94@uni-bonn.de>
2023-06-15 21:39:50 +0200

    with new orientation

--- santoshproject.R
+++ santoshproject.R
@@ -5,27 +5,22 @@ library(DiagrammeR)
 DiagrammeR::grViz("digraph {
   
 graph[layout = dot, rankdir = RR]
+node[style=filled]
 
-1 [label = 'Raw materials']
-2 [label = ' Added Sugar', shape = Rectangle, fillcolor = Biege]
 3 [label = 'SUGAR TAX', shape = triangle]
 4 [label = 'Outcome']
-5 [label = 'Good', shape = hexagon]
-6 [label = 'Bad', shape = hexagon]
-7 [label = 'Sugar Beverages']
-8 [label = 'NO SUGAR TAX', shape = triangle]
+5 [label = 'Good', shape = hexagon, fillcolor= 'green']
+6 [label = 'Bad', shape = hexagon, fillcolor = 'firebrick']
+7 [label = 'Decision', shape = cylinder, fontsize= 24, style='bold']
+8 [label = 'NO SUGAR TAX', shape = invtriangle]
 9 [label = 'Outcome']
-10 [label = 'Good', shape = hexagon, colorfill = green]
-11 [label = 'Bad', shape = hexagon]
-12 [label = 'Stakeholders']
+10 [label = 'Good', shape = hexagon, fillcolor = 'green']
+11 [label = 'Bad', shape = hexagon, fillcolor = 'firebrick']
 
-
-1 -> 2 -> 7
           7-> 3->4 -> 5, 6
           7-> 8-> 9 -> 10, 11
-          5->3
-          6->8 
-          10->8
-          11->3
-          12->8,3
+          5->3 [color= 'green']
+          6->8 [style= 'dashed', color='firebrick']
+          10->8 [color = 'green']
+          11->3 [style= 'dashed', color='firebrick']
 }")
